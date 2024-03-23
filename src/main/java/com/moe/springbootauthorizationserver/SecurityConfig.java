de5910488f7fe8c4ea1651ead825e9766003b89c
mahmoudajam <mahmoud.othman.ajam@gmail.com>
2024-03-23 20:54:13 +0300

    Initial commit

---
+++ src/main/java/com/moe/springbootauthorizationserver/SecurityConfig.java
@@ -0,0 +1,154 @@
+package com.moe.springbootauthorizationserver;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain asFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration
+                .applyDefaultSecurity(http);
+
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(Customizer.withDefaults());
+
+        http.exceptionHandling((e) ->
+                e.authenticationEntryPoint(
+                        new LoginUrlAuthenticationEntryPoint("/login")
+                ));
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin(Customizer.withDefaults());
+
+        http.authorizeHttpRequests(
+                c -> c.anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+
+    // Todo: Implement user saving and retrieving in the way you like (usually in a Database)
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withUsername("moe")
+                .password("12345")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+
+    // Todo : Implement the password encoding method of your choosing
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
+    /**
+     *
+     * “A client ID - external client identifier similar to what a username is for the user.
+     * A client secret – similar to what a password is for a user.
+     * The client authentication method – tells how the authorization server expects the client to authenticate when sending requests for access tokens.
+     * Authorization grant type – A grant type allowed by the authorization server for this client. A client might use multiple grant types.
+     * Redirect URI – One of the URI addresses the authorization server allows the client to request a redirect for providing the authorization code in case of the authorization code grant type.
+     * A scope – Defines a purpose for the request of an access token. The scope can be used later in authorization rules.”
+     *
+     */
+    // Todo : In normal applications you should implement this to save the details in the database and retrieve it from there.
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient registeredClient =
+                RegisteredClient
+                        .withId(UUID.randomUUID().toString())
+                        .clientId("client")
+                        .clientSecret("secret")
+                        .clientAuthenticationMethod(
+                                ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                        .authorizationGrantType(
+                                AuthorizationGrantType.AUTHORIZATION_CODE)
+                        .redirectUri("https://www.manning.com/authorized")
+                        .scope(OidcScopes.OPENID)
+                        .build();
+
+        return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+
+
+    /**
+     *
+     * Key Pair Management Configuration
+     * In our case every time a deployment happens the keys that have been already issued will not work anymore, which is not ideal for a real world app
+     * Consider securely saving the keys somewhere in a database
+     *
+     */
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+
+        keyPairGenerator.initialize(2048);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+
+        RSAKey rsaKey = new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    /**
+     *
+     * Allows to customize all the endpoint paths that the authorization server exposes
+     *
+     */
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings() {
+        return AuthorizationServerSettings.builder().build();
+    }
+}
---
+++ src/test/java/com/moe/springbootauthorizationserver/SpringBootAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moe.springbootauthorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
